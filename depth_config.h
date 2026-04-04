48f9ad7dc84a032049f1efdfbb96735fe329644a
Liuenne <eltonchen2004@gmail.com>
2026-04-03 21:31:21 -0700

    depth config and stats header files

---
+++ depth_config.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "instruction.h"
+
+double get_freq(int D) {
+    switch(D) {
+        case 1: return 2.0;
+        case 2: return 1.5;
+        case 3: return 1.0;
+        default: return 1.0;
+    }
+}
+
+void depth_config(instruction& inst, int D) {
+    if (inst.type == FP && (D == 2 || D == 4)) {
+        inst.ex_cycles = 2;
+    }
+
+    if (inst.type == LOAD && (D == 3 || D == 4)) {
+        inst.mem_cycles = 3;
+    }
+}
---
+++ stat.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "instruction.h"
+#include "depth_config.h"
+#include <iostream>
+#include <iomanip>
+
+struct stats {
+    long long total_cycles;
+    double exec_time = 0.0; //ms
+    int count[6] = {0}; //count of each instruction type
+};
+
+void record_inst(const instruction& inst, stats& s) {
+    s.count[inst.type]++;
+
+    if (inst.cycle_wb + 1 > s.total_cycles) {
+        s.total_cycles = inst.cycle_wb + 1;
+    }
+}
+
+void finalize_stats(stats& s, int D) {
+    double freq = get_freq(D);
+    double cycle_time = 1.0 / (freq * 1e6);
+    s.exec_time = s.total_cycles * cycle_time; //ms
+}
+
+void print_stats(const stats& s, int D, long long start_inst, long long inst_count) {
+    long long total = 0;
+    for (int i = 1; i <= 5; i++) total += s.count[i];
+
+    std::cout << std::fixed << std::setprecision(4);
+    std::cout << "========================================\n";
+    std::cout << "D = " << D
+              << "start = " << start_inst
+              << "count = " << inst_count << "\n";
+    std::cout << "Total Cycles: " << s.total_cycles << "\n";
+    std::cout << "Exec Time (ms): " << s.exec_time << "\n";
+    std::cout << "----------------------------------------\n";
+
+    if (total == 0) {
+        std::cout << "No instructions executed." << std::endl;
+        return;
+    }
+
+    std::cout << "INT: " << s.count[INT]    << "(" << 100.0 * s.count[INT]    / total << "%)\n";
+    std::cout << "FP: " << s.count[FP]     << "(" << 100.0 * s.count[FP]     / total << "%)\n";
+    std::cout << "MEM: " << s.count[MEM]    << "(" << 100.0 * s.count[MEM]    / total << "%)\n";
+    std::cout << "LOAD: " << s.count[LOAD]   << "(" << 100.0 * s.count[LOAD]   / total << "%)\n";
+    std::cout << "STORE: " << s.count[STORE]  << "(" << 100.0 * s.count[STORE]  / total << "%)\n";
+    std::cout << "========================================\n";
+}

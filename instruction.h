581812c3f0f64c03dde831ffcdfd5397e7c4a43f
Mouryan Puri <parjun000000@gmail.com>
2026-04-13 12:39:13 -0700

    inialized all variable to starter value

--- instruction.h
+++ instruction.h
@@ -7,13 +7,18 @@
 enum InstType { INT=1, FP=2, BRANCH=3, LOAD=4, STORE=5 };
 
 struct instruction {
-    uint64_t pc;
-    InstType type;
-    std::vector<uint64_t> dep_pc; //dependant instructions
-    
-    long long cycle_if, cycle_id, cycle_ex, cycle_mem, cycle_wb;
-    int ex_cycles =1; //cycles spent in EX
-    int mem_cycles =1; //cycles spent in MEM
+    uint64_t pc = 0;
+    InstType type = INT;
+    std::vector<uint64_t> dep_pc;
+
+    long long cycle_if = -1;
+    long long cycle_id = -1;
+    long long cycle_ex = -1;
+    long long cycle_mem = -1;
+    long long cycle_wb = -1;
+
+    int ex_cycles = 1;
+    int mem_cycles = 1;
 };
 
 #endif
